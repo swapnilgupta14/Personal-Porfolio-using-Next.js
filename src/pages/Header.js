c3c069288b46ba994490ccd46d4b7e366cd12a19
Swapnil Gupta <85231522+swapnilgupta14@users.noreply.github.com>
2024-05-27 16:13:33 +0530

    test

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -1,3 +1,4 @@
+import { Router } from 'next/router';
 import React from 'react';
 
 const Header = () => {
@@ -15,7 +16,7 @@ const Header = () => {
                     </div>
                     <div className="">
                         <ul className="flex space-x-8 text-lg text-white-800 font-medium">
-                            <li className="hover:text-blue-600 transition duration-300 ease-in-out cursor-pointer">Home</li>
+                            <li className="hover:text-blue-600 transition duration-300 ease-in-out cursor-pointer" onClick={Router.push('/Page')}>Home</li>
                             <li className="hover:text-blue-600 transition duration-300 ease-in-out cursor-pointer">About</li>
                             <li className="hover:text-blue-600 transition duration-300 ease-in-out cursor-pointer">Projects</li>
                             <li className="hover:text-blue-600 transition duration-300 ease-in-out cursor-pointer">Contact</li>
