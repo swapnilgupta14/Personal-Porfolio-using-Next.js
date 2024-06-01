77bd532b079361f3c1547d05d13e78edf3381057
Swapnil Gupta <85231522+swapnilgupta14@users.noreply.github.com>
2024-06-01 19:55:54 +0530

    update :: changes loader screen

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
@@ -7,9 +8,50 @@ import Header from "@/component/header";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const [showWelcome, setShowWelcome] = useState(true);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setShowWelcome(false);
+    }, 2000);
+    return () => clearTimeout(timeout);
+  }, []);
+
   return (
     <>
-      <div><Header/></div>
+      <div>
+
+        {showWelcome ? (
+          <div>
+            <div className="loader-container">
+              <div className="loader">
+                <svg viewBox="0 0 80 80">
+                  <circle cx="40" cy="40" r="32"></circle>
+                </svg>
+              </div>
+              <div className="loader triangle">
+                <svg viewBox="0 0 86 80">
+                  <polygon points="43 8 79 72 7 72"></polygon>
+                </svg>
+              </div>
+              <div className="loader">
+                <svg viewBox="0 0 80 80">
+                  <rect x="8" y="8" width="64" height="64"></rect>
+                </svg>
+              </div>
+            </div>
+          </div>
+        ) : (
+          <Header />
+        )}
+        {!showWelcome && (
+          <style jsx global>{`
+            .hide-welcome-message {
+              display: none !important;
+            }
+          `}</style>
+        )}
+      </div>
     </>
   );
 }
