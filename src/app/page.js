cd185fcc8c94d6b8e3569f118c0f4ee1f26631e4
Swapnil Gupta <85231522+swapnilgupta14@users.noreply.github.com>
2024-06-01 07:40:36 +0530

    new app

--- src/app/page.js
+++ src/app/page.js
@@ -1,86 +1,95 @@
-'use client';
-import { useEffect, useState } from 'react';
-import HomePage from '../pages/HomePage';
-import Header from '../pages/Header';
+import Image from "next/image";
+import styles from "./page.module.css";
 
 export default function Home() {
-  const [position, setPosition] = useState({ x: 0, y: 0 });
-  const [visible, setVisible] = useState(false);
-
-  useEffect(() => {
-    let moveTimeoutId;
-    let hideTimeoutId;
-
-    const showCursor = () => {
-      clearTimeout(hideTimeoutId);
-      setVisible(true);
-    };
-
-    const hideCursor = () => {
-      hideTimeoutId = setTimeout(() => {
-        setVisible(false);
-      }, 200);
-    };
-
-    const mouseMoveHandler = (event) => {
-      clearTimeout(moveTimeoutId);
-      showCursor();
-      setPosition({ x: event.clientX, y: event.clientY });
-      moveTimeoutId = setTimeout(() => {
-        hideCursor();
-      }, 200);
-    };
-
-    const mouseEnterHandler = () => {
-      showCursor();
-    };
-
-    const mouseLeaveHandler = () => {
-      hideCursor();
-    };
-
-    document.addEventListener('mousemove', mouseMoveHandler);
-    document.addEventListener('mouseenter', mouseEnterHandler);
-    document.addEventListener('mouseleave', mouseLeaveHandler);
-
-    return () => {
-      clearTimeout(moveTimeoutId);
-      clearTimeout(hideTimeoutId);
-      document.removeEventListener('mousemove', mouseMoveHandler);
-      document.removeEventListener('mouseenter', mouseEnterHandler);
-      document.removeEventListener('mouseleave', mouseLeaveHandler);
-    };
-  }, []);
-
   return (
-    <>
-      <style jsx global>{`
-        body {
-          cursor: none;
-        }
-      `}</style>
-      <div>
-        <Header />
-      </div>
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <HomePage />
-        <div
-          style={{
-            left: `${position.x}px`,
-            top: `${position.y}px`,
-            opacity: visible ? 1 : 0,
-            transition: 'opacity 0.2s ease-in-out',
-          }}
-          className="fixed w-8 h-8 transform -translate-x-1/2 -translate-y-1/2 pointer-events-none"
-        >
-          <div className="relative w-full h-full">
-            <div className="absolute inset-0 flex items-center justify-center">
-              <div className="w-2 h-2 bg-gradient-to-r from-pink-500 to-yellow-500 rounded-full"></div>
-            </div>
-            <div className="absolute inset-0 border-2 border-white rounded-full"></div>
-          </div>
+    <main className={styles.main}>
+      <div className={styles.description}>
+        <p>
+          Get started by editing&nbsp;
+          <code className={styles.code}>src/app/page.js</code>
+        </p>
+        <div>
+          <a
+            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            By{" "}
+            <Image
+              src="/vercel.svg"
+              alt="Vercel Logo"
+              className={styles.vercelLogo}
+              width={100}
+              height={24}
+              priority
+            />
+          </a>
         </div>
-      </main>
-    </>
+      </div>
+
+      <div className={styles.center}>
+        <Image
+          className={styles.logo}
+          src="/next.svg"
+          alt="Next.js Logo"
+          width={180}
+          height={37}
+          priority
+        />
+      </div>
+
+      <div className={styles.grid}>
+        <a
+          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>
+            Docs <span>-&gt;</span>
+          </h2>
+          <p>Find in-depth information about Next.js features and API.</p>
+        </a>
+
+        <a
+          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>
+            Learn <span>-&gt;</span>
+          </h2>
+          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
+        </a>
+
+        <a
+          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>
+            Templates <span>-&gt;</span>
+          </h2>
+          <p>Explore starter templates for Next.js.</p>
+        </a>
+
+        <a
+          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>
+            Deploy <span>-&gt;</span>
+          </h2>
+          <p>
+            Instantly deploy your Next.js site to a shareable URL with Vercel.
+          </p>
+        </a>
+      </div>
+    </main>
   );
 }
